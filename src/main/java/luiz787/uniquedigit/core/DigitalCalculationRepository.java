a3d20d9f1cd525952fbd97fc8442d1f7accfee54
Matheus Magalhaes <matheusibrahimfm@gmail.com>
2021-12-01 16:20:05 -0300

    Add respository and controller to Digit Calculator

---
+++ src/main/java/luiz787/uniquedigit/core/DigitalCalculationRepository.java
@@ -0,0 +1,6 @@
+package luiz787.uniquedigit.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniqueDigitCalculationRepository
+    extends JpaRepository<UniqueDigitCalculation, Long> {}
