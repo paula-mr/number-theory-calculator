06935fc7e36e751bbc0dadaa161a01bb8b6bef19
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-12-02 22:07:28 -0300

    Test parse with empty string

--- src/test/java/calculator/utils/NumberUtilsTest.java
+++ src/test/java/calculator/utils/NumberUtilsTest.java
@@ -23,6 +23,16 @@ public class NumberUtilsTest {
                 "the supplied string \"null\" is not a valid number");
     }
 
+        public void parse_GivenSIsEmpty_ShouldThrowIllegalArgumentException(final String s) {
+        final String position = "";
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> NumberUtils.parse(position),
+                "the supplied string \"null\" is not a valid number");
+    }
+
+
     public void parse_GivenSIsNegative_ShouldReturnNegativeNumber(final String s) {
         final String position = "-1";
 
