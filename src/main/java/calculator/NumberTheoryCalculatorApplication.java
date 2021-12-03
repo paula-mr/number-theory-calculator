7982d98d16e8a816540030caca39fe09e5f559a1
Victor Moraes <victorgabrielvgfm@gmail.com>
2021-12-02 22:49:38 -0300

    Renames application main class

--- src/main/java/calculator/NumberTheoryCalculatorApplication.java
+++ src/main/java/calculator/NumberTheoryCalculatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniquedigitApplication {
+public class NumberTheoryCalculatorApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(UniquedigitApplication.class, args);
+    SpringApplication.run(NumberTheoryCalculatorApplication.class, args);
   }
 }
--- src/test/java/calculator/NumberTheoryCalculatorApplicationTests.java
+++ src/test/java/calculator/NumberTheoryCalculatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UniquedigitApplicationTests {
+class NumberTheoryCalculatorApplicationTests {
 
   @Test
   void contextLoads() {}
